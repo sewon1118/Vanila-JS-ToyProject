9862bee64c163b968fb6a33b53be3735a22ce511
DonghyunKim98 <akainoo@naver.com>
2020-10-02 22:09:19 +0900

    feat #2 : 숫자야구 보완
    - inner Text에 게임한 기록 보여줌
    - Computer Num 은 더이상 중복된 숫자를 가지지 않음
    - 시작을 눌러야 게임을 할 수 있게함

--- 1/App.js
+++ 1/App.js
@@ -1,45 +1,62 @@
-'use strict';
-const input=document.getElementById('inputNum');
-const result=document.getElementById('result');
-
-let getRndInteger=(min, max)=>{
-    return Math.floor(Math.random() * (max - min) ) + min;
+"use strict";
+const input = document.getElementById("inputNum");
+const result = document.getElementById("result");
+const restartBtn = document.getElementById("startBtn");
+const getRndInteger = (n) => {
+    const digits = Array.from({ length: 10 }, (_, i) => i);
+    const ret = Array.from({ length: n }, () => {
+    const randIndex = Math.floor(Math.random() * digits.length);
+    const digit = digits[randIndex];
+    digits.splice(randIndex, 1);
+    return digit;
+  });
+  return Number(ret.join(""));
 };
 
-let compare=(num1,num2)=>{
-    console.log(num1,num2);
-    let map1=new Map(), map2=new Map();
-    for(let i=0;i<4;i++){
-        map1.set(i,num1%10);
-        map2.set(i,num2%10);
-        num1=Math.floor(num1/10), num2=Math.floor(num2/10);
-    }
-    let strike=0, ball=0;
-    //using call back
-    map1.forEach((value,key)=>{
-        let v1 = value;
-        let k1 = key;
-        map2.forEach((value,key)=>{
-            if(v1===value){
-                if(key===k1) strike++;
-                else ball++;
-            }
-        }
-        )
-    })
-    return {strike,ball};
-}
-let comNum = getRndInteger(1000,9999);
+const compare = (num1, num2) => {
+  let strike = 0,
+    ball = 0;
+  console.log(num1, num2);
+  let arr1 = num1.toString(10).split("").map(Number);
+  let arr2 = num2.toString(10).split("").map(Number);
+  arr1.forEach((v1, i1) => {
+    arr2.forEach((v2, i2) => {
+      if (v1 === v2) {
+        if (i1 === i2) strike++;
+        else ball++;
+        return;
+      }
+    });
+  });
+  return { strike, ball };
+};
 
-input.addEventListener("keyup",(event)=>{
-    if(event.keyCode===13){
-        let inputNum=parseInt(input.value);
-        console.log(typeof(inputNum))
-        let a=compare(inputNum,comNum);
-        if(a.strike==0 && a.ball==0) result.innerHTML="OUT!";
-        else if(a.strike==4) 
-            result.innerHTML="숫자를 맞혔습니다!";
-        else result.innerHTML=`${a.strike}S ${a.ball}B`;
-        return false;
+let comNum;
+const startBtnOnClickListener = () => {
+  if (input.disabled === true) input.disabled = false;
+  result.innerText="";
+  comNum = getRndInteger(4);
+};
+restartBtn.onclick = startBtnOnClickListener;
+
+
+
+input.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    let inputNum = input.value;
+    input.value="";
+    let res = compare(inputNum, comNum);
+    let tempText = `${inputNum}: `;
+    if (res.strike == 0 && res.ball == 0) tempText += "OUT!";
+    else if (res.strike == 4) {
+        tempText += "숫자를 맞혔습니다!";
+        input.disabled=true;
     }
-})
+    else tempText += `${res.strike}S ${res.ball}B`;
+    tempText+="\r\n";
+    result.innerText += tempText;
+    return false;
+  }
+});
+
+//IIFE -> 즉시 함수를 호출하고 싶을때
