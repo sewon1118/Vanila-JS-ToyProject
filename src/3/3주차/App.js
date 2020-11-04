fc601b413fb21c2f38756342cb0b20322bf56dd0
동현 김 <akainoo@naver.com>
2020-11-04 13:08:50 +0900

    feat #11 : 3주차 지뢰찾기 2차 구현
    - 모든 구현 완료

--- src/3/3주차/App.js
+++ src/3/3주차/App.js
@@ -1,25 +1,20 @@
 "use strict";
 /*
-  리팩토링 해야하는 사항
-  1. data와 logic의 엄격한 구별
-  2. 게임 패베와 승리에 대한 promise를 이용한 비동기적인 코드 작성
-  3. 필요없고 겹치는 부분 삭제
-  4. 게임 실패시 타이머가 멈춰야함
-
   삽질 한 부분
-  1. 2차원 배열 생성 부분
-  2. 왼쪽 클릭 리스너 생성 부분
-    => contextmenu 라는 method를 호출해야 했던 것. 클릭에서 분기처리를 하는 것이 아닌
-  3. visibility 부분 
-    => 구현 방법 : childNode를 가져와서 그것의 visiblity를 visible로 만들기
-    => childNode 를 가져오면 Nodelist 형태이므로 배열의 첫번째를 가져오는 형태여야 함
-  4. grid의 id를 가져올려고 함 => 매우 어려움 (addEventListener로 구현하기 어려움)
-    => className으로 가져오면 편함
-    => grid안의 Text를 div 처리한지를 까먹고, Text에 class를 부여했다가 삽질을 했음
-    => 또한 innerText를 ' '로 설정했더니 condition operator가 정상작동하지 않음
-      => innerText를 0으로 설정하고, 0인경우엔 innerText의 visibility를 hidden으로 유지하게 구현
-  5. js에선 Number의 type밖에 없으므로, integer와 integer를 나눈다고 해서 integer가 나오지 않음
-    => Math.floor() 로 구현
+  1. domData 관련
+  => page가 upload되면 onStartBtn쪽에 있는 promise가 그냥 실행이 된다 왜지??
+    => 개념 숙지 부족,,, promise는 생성 즉시 자동으로 실행이 된다
+    =>
+  => 그리고 StartBtn을 누른후에도 여전히 nav는 못찾는중
+  => row와 col도 마찬가지
+
+  2. JS에서의 함수 호이스팅
+  => 보통은 다 된다.
+  => 그렇지만 함수 리터럴 / Function 생성자 / 화살표 함수 표현식 으로 정의한 함수는 그 참조를 변수에 할당한 후에 호출 가능
+  => 책 256쪽 참고.
+
+  3. Promise 반환 하는 부분
+  => 함수가 Promise 자체를 반환하기때문에, 그 함수에서 then과 catch를 이어나가야 한다.
 */
 // 기본 Data
 const level = {
@@ -28,15 +23,17 @@ const level = {
   고급: [20, 20, 40],
   "사용자 설정": null,
 };
-let row, col;
+const width =300, height=300;
 let gameData = {
   selectLevel: null,
   flagCnt: 0,
   bombCnt: 0,
   time: 0,
   blankCnt: 0,
+  row: 0,
+  col: 0,
 };
-let DomData={};
+let domData={};
 let dir = [
   [0, 1],
   [0, -1],
@@ -65,7 +62,7 @@ const gameDataProxy = new Proxy(gameData, {
     return name in target ? target[name] : -1;
   }
 });
-const domDataProxy =new Proxy(DomData,{
+const domDataProxy =new Proxy(domData,{
   set:(target,key,value)=>{
     target[key]=value;
     return true;
@@ -83,7 +80,7 @@ const App = (parent) => {
 const setDefault = ()=>{
   // levelSelect 
   domDataProxy.levelSelect = document.getElementById('level_select');
-  domDataProxy.levelSelect.onchange=onLevelChange();
+  domDataProxy.levelSelect.addEventListener("change",onLevelChange,false);
   for(let key in level){
     let levelElt = elt("option",null,key);
     domDataProxy.levelSelect.appendChild(levelElt);
@@ -129,8 +126,7 @@ const createCustomField =()=>{
   customField.appendChild(customHeight);
   customField.appendChild(customBomb);
 
-  document.body.appendChild(customField);
-  domDataProxy.customField=document.getElementById('custom_Field');
+  domDataProxy.nav.appendChild(customField);
 }
 
 const onLevelChange = () => {
@@ -138,83 +134,62 @@ const onLevelChange = () => {
 };
 
 // 2단계: set&createGame 
-const setGame = new Promise((resolve,reject)=>{
-  // ** 이 부분을 자동적으로 처리할 수 있게 만드는 코드가 필요
-  gameDataProxy.selectLevel=domDataProxy.levelSelect.value;
-  if(gameDataProxy.selectLevel==="사용자 설정"){
-    row = parseInt(domDataProxy.rowInput.value);
-    col = parseInt(domDataProxy.heightInput.value);
-    gameDataProxy.bombCnt=parseInt(domDataProxy.bombInput.value);
-  } else{
-    row = level[selectLevel][0];
-    col = level[selectLevel][1];
-    gameDataProxy.bombCnt=level[selectLevel][2];
-  }
-  if (row*col<gameDataProxy.bombCnt) reject(new Error("폭탄이 너무 많습니다!"));
-  else resolve();
-});
+const setGame = ()=>{
+  return new Promise((resolve,reject)=>{
+    let info = elt("div",{id:"game_info"});
+    let flag = elt("div", { id: "flag_data" }, `🚩 ${gameDataProxy.flagCnt}`);
+    let bomb = elt("div", { id: "bomb_data" }, `💣 ${gameDataProxy.bombCnt}`);
+    let timer = elt("div", { id: "time_data" }, `시간 ${gameDataProxy.time}`);
+    info.appendChild(flag);
+    info.appendChild(bomb);
+    info.appendChild(timer);
+    domDataProxy.nav.appendChild(info);
 
-// ** width와 height를 받아올 수 있게 만들어야함
-const createGame = new Promise((resolve)=>{
-  //selectLevel이 있을 경우
-  if(domDataProxy.levelSelect!=-1)
-    domDataProxy.levelSelect.remove();
-  let game= elt("table",null);
-  const trWidth = width/ gameDataProxy.col;
-  const trHegiht = height / gameDataProxy.row;
-  const bombArr = createBomb(gameDataProxy.row,gameDataProxy.col,gameDataProxy.bombCnt);
-  for(let ypos=0;ypos<gameDataProxy.row;ypos++){
-    let tr = elt("tr",{height:`${trHegiht}px`})
-    for(let xpos=0;xpos<gameDataProxy.col;xpos++){
-      let element = elt("td",{
-        width: `${trWidth}px`,
-        id: `${ypos}_${xpos}`,
-      });
-      let elText = elt("span",{
-        value: `${bombArr[ypos][xpos]}`
-      });
-      element.appendChild(elText);
-      element.addEventListener("click",gridClickListener,false);
-      element.addEventListener("contextmenu",gridRightClickListener,false);
-      row.appendChild(element);
+    // ** 이 부분을 자동적으로 처리할 수 있게 만드는 코드가 필요
+    gameDataProxy.selectLevel=domDataProxy.levelSelect.value;
+    if(gameDataProxy.selectLevel==="사용자 설정"){
+      gameDataProxy.row = parseInt(domDataProxy.rowInput.value);
+      gameDataProxy.col = parseInt(domDataProxy.heightInput.value);
+      gameDataProxy.bombCnt=parseInt(domDataProxy.bombInput.value);
+    } else{
+      gameDataProxy.row = level[gameDataProxy.selectLevel][0];
+      gameDataProxy.col = level[gameDataProxy.selectLevel][1];
+      gameDataProxy.bombCnt=level[gameDataProxy.selectLevel][2];
     }
-  }
+    if (gameDataProxy.row*gameDataProxy.col<gameDataProxy.bombCnt) reject(new Error("폭탄이 너무 많습니다!"));
+    else {
+      gameDataProxy.flagCnt=gameDataProxy.bombCnt;
+      resolve();
+    }
+    })
+};
 
-  let info = elt("div",{id:"game_info"});
-  let flag = elt("div", { id: "flag_data" }, `🚩 ${gameDataProxy.flagCnt}`);
-  let bomb = elt("div", { id: "bomb_data" }, `💣 ${gameDataProxy.bombCnt}`);
-  let timer = elt("div", { id: "time_data" }, `시간 ${gameDataProxy.time}`);
-  info.appendChild(flag);
-  info.appendChild(bomb);
-  info.appendChild(timer);
-
-  resolve({game,info});
-})
-
-const createBomb = () => {
+const createBomb =  () => {
   let temp = new Array();
   for (let i = 0; i < gameDataProxy.bombCnt; i++) temp.push("💣");
-  for (let i = 0; i < row * col - gameDataProxy.bombCnt; i++) temp.push("0");
+  for (let i = 0; i < gameDataProxy.row * gameDataProxy.col - gameDataProxy.bombCnt; i++) temp.push("0");
   // shuffle
   for (let i = temp.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [temp[i], temp[j]] = [temp[j], temp[i]];
   }
-  let ret = Array.from(Array(row), () => new Array(col));
-  temp.forEach((value, idx) => (ret[Math.floor(idx / row)][idx % row] = value));
+  // ** promise 자체에 대한 미숙한 사용때문에 계속해서 proxy에서 받아오는것이 잘 안됨
+  let ret = Array.from(Array(gameDataProxy.row), () => new Array(gameDataProxy.col));
+  temp.forEach((value, idx) => (ret[Math.floor(idx / gameDataProxy.row)][idx % gameDataProxy.row] = value));
   processBomb(ret);
   return ret;
 };
 
+
 const processBomb = (arr) => {
-  for (let i = 0; i < row; i++)
-    for (let j = 0; j < col; j++) {
+  for (let i = 0; i < gameDataProxy.row; i++)
+    for (let j = 0; j < gameDataProxy.col; j++) {
       if (arr[i][j] === "💣") continue;
       let cnt = 0;
       dir.forEach((value) => {
         let ny = i + value[0],
           nx = j + value[1];
-        if (0 <= ny && ny < row && 0 <= nx && nx < col)
+        if (0 <= ny && ny < gameDataProxy.row && 0 <= nx && nx < gameDataProxy.col)
           if (arr[ny][nx] == "💣") cnt++;
       });
       arr[i][j] = cnt ? `${cnt}` : "0";
@@ -222,14 +197,49 @@ const processBomb = (arr) => {
 };
 
 
+// ** width와 height를 받아올 수 있게 만들어야함
+const createGame = ()=>{
+  return new Promise((resolve)=>{
+    let game= elt("table",null);
+    const trWidth = width/ gameDataProxy.col;
+    const trHegiht = height / gameDataProxy.row;
+    // ** 리팩토링이 필요한 부분
+    // ** Promise에 있는 함수는 뒤에 있으면 init이 안되있는걸로 간주, 찾지 못함
+    const bombArr =  createBomb();
+    for(let ypos=0;ypos<gameDataProxy.row;ypos++){
+      let tr = elt("tr",{height:`${trHegiht}px`})
+      for(let xpos=0;xpos<gameDataProxy.col;xpos++){
+        let element = elt("td",{
+          width: `${trWidth}px`,
+          id: `${ypos}_${xpos}`,
+        });
+        let elText = elt("span",{
+          value: `${bombArr[ypos][xpos]}`
+        });
+        element.appendChild(elText);
+        element.addEventListener("click",gridClickListener,false);
+        element.addEventListener("contextmenu",gridRightClickListener,false);
+        tr.appendChild(element);
+      }
+      game.appendChild(tr);
+    }
+    resolve(game);
+  })
+}
+
+
+
+
+
 // ** 사실 게임 종료 조건이 즉각적이진 않음 (10ms마다 반응하긴 하지만)
 // ** 또한 매우 메모리를 많이 잡아먹기도함.
-const playGame= new Promise((resolve,reject)=>{
+const playGame= ()=>{
+  return new Promise((resolve,reject)=>{
   let timeSet = setInterval(()=>{
       gameDataProxy.time++;
     },1000);
   let checkGameOver = setInterval(()=>{
-    if (gameDataProxy.bombCnt == row * col - gameDataProxy.blankCnt){
+    if (gameDataProxy.bombCnt == gameDataProxy.row * gameDataProxy.col - gameDataProxy.blankCnt){
       clearInterval(timeSet);
       clearInterval(checkGameOver);
       resolve();
@@ -240,7 +250,8 @@ const playGame= new Promise((resolve,reject)=>{
       reject();
     }
   },10);
-})
+  })
+};
 
 // 그리드 클릭 리스너
 const gridClickListener = (e) => {
@@ -249,12 +260,13 @@ const gridClickListener = (e) => {
   e.currentTarget.style.backgroundColor = "white";
   let text = e.currentTarget.childNodes[0];
   gameDataProxy.blankCnt++;
-  text.innerText=text.value;
+  text.innerText=text.getAttribute('value');
   if (text.innerText === "💣") {
     gameDataProxy.isGameOver=false;
     return false;
   } 
   else if (text.innerText === "0") {
+    text.innerText=' ';
     let idName = e.currentTarget.id.split("_");
     let ypos = parseInt(idName[0]);
     let xpos = parseInt(idName[1]);
@@ -263,12 +275,10 @@ const gridClickListener = (e) => {
     dir.forEach((value) => {
       let ny = value[0] + ypos,
         nx = value[1] + xpos;
-      if (0 <= ny && ny < row && 0 <= nx && nx < col) {
-        let nextNode = document.getElementById(`${ny}_${nx}`)[0];
-        if (nextNode.childNodes[0].value != "💣")
+      if (0 <= ny && ny < gameDataProxy.row && 0 <= nx && nx < gameDataProxy.col) {
+        let nextNode = document.getElementById(`${ny}_${nx}`);
+        if (nextNode.childNodes[0].getAttribute('value') != "💣")
           nextNode.dispatchEvent(event);
-        if (nextNode.childNodes[0].value != "0")
-          nextNode.childNodes[0].innerText = nextNode.childNodes[0].value;
       }
     });
   } 
@@ -284,7 +294,7 @@ const gridRightClickListener = (e) => {
 };
 
 
-// 3단계 : 
+// 3단계 : 게임 종료
 const SuccessGame=()=>{
   alert("모든 폭탄을 찾았습니다!! 대단해요!");
 }
@@ -295,14 +305,22 @@ const FailGame=()=>{
 
 // 1->2->3단계 : 스타트 버튼
 const onStartBtnClickListener =  () => {
-  setGame
-    .then(()=> createGame)
-    .catch((error)=>alert(error))
-    .then(({game,info})=> {
-      document.bodu.appendChild(domDataProxy.info=info);
-      document.body.appendChild(domDataProxy.section=elt("section",null,game));
+  setGame()
+    .then(()=> {
+      createGame()
+      .then((game)=> {
+        domDataProxy.section=elt("section",null,game);
+        document.body.appendChild(domDataProxy.section);
+      })
+      .then(()=> playGame())
+      .then(()=> SuccessGame())
+      .catch((e)=> {
+        console.log(e);
+        FailGame()
+      });
     })
-    .then(()=> playGame)
-    .then(()=> SuccessGame)
-    .catch(()=> FailGame);
+    .catch((error)=>{
+      console.log(error);
+      alert(error);
+    })
 };
