7ea48ccc76751f2f7140c838e79a5f58c1fc62d3
동현 김 <akainoo@naver.com>
2020-11-12 15:20:45 +0900

    feat #2: 4주차 Clcok 2차 구현
    - Clock CSS

--- src/4/Clock/ClockData.js
+++ src/4/Clock/ClockData.js
@@ -6,7 +6,7 @@ export const setData=()=>{
         const minutes = now.getMinutes();
         const hours = now.getHours();
         setDigitalClock(seconds,minutes,hours);
-        setRoundClock()
+        setRoundClock(seconds,minutes,hours%12);
         },1000);
 }
     
@@ -18,5 +18,16 @@ const setDigitalClock=(seconds,minutes,hours)=>{
 }   
 
 const setRoundClock=(seconds,minutes,hours)=>{
-    const roundClock=document.getElementById('round_clock');
+    const secHand=document.getElementById('second_hand');
+    const minHand=document.getElementById('minute_hand');
+    const hourHand=document.getElementById('hour_hand');
+
+    const secondsDegrees=((seconds/60)*360)+90;
+    secHand.style.transform=`rotate(${secondsDegrees}deg)`;
+
+    const minsDegrees = ((minutes/60)*360+(seconds/60)*6)+90;
+    minHand.style.transform=`rotate(${minsDegrees}deg)`;
+    
+    const hoursDegrees = ((hours/12)*360+(minutes/60)*30)+90;
+    hourHand.style.transform=`rotate(${hoursDegrees}deg)`;
 }
